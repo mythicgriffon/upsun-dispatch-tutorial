f620236b87d5dde12060bff20d7c330affc34323
Corey Dockendorf <mythicgriffon@gmail.com>
2026-09-21 13:37:11 -0500

    Optimize reading time calculation in ArticleRepository
    Updated return max count

--- src/Blog/ArticleRepository.php
+++ src/Blog/ArticleRepository.php
@@ -164,7 +164,7 @@ private function readingTime(string $markdown): int
     $words = str_word_count(strip_tags($markdown));
 
     // Round up so a partial minute still counts as a full minute.
-    return max(1, intdiv($words, self::WORDS_PER_MINUTE) + 1);
+    return max(1, intdiv($words + self::WORDS_PER_MINUTE - 1, self::WORDS_PER_MINUTE));
 }
 
     private function converter(): MarkdownConverter
